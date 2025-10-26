959c2a948bed1315906a5a922116d5a674b7d614
hannah sky <165714275+hannahskyg@users.noreply.github.com>
2025-10-25 17:36:57 -0700

    lab 4 and step 3

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 import { fetchJSON } from "../global.js";
 import { renderProjects } from "../global.js";
+import { fetchGitHubData } from "../global.js";
 const projects = await fetchJSON('../lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
+const githubData = await fetchGitHubData('hannahskyg');
+const profileStats = document.querySelector('#profile-stats');
+
+if (profileStats && githubData) {
+  profileStats.innerHTML = `
+        <dl>
+          <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+          <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+          <dt>Followers:</dt><dd>${githubData.followers}</dd>
+          <dt>Following:</dt><dd>${githubData.following}</dd>
+          <dt>Created At:</dt><dd>${new Date(githubData.created_at).toLocaleDateString()}</dd>
+        </dl>
+    `;
+}
