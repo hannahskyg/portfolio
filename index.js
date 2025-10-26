2a7ecccb37c4bfb7434a73fed1b2408f322ecc9f
hannah sky <165714275+hannahskyg@users.noreply.github.com>
2025-10-25 20:10:50 -0700

    Trying to fix global and index js issues

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import { fetchJSON } from "../global.js";
-import { renderProjects } from "../global.js";
-import { fetchGitHubData } from "../global.js";
-const projects = await fetchJSON('../lib/projects.json');
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
+
+const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
